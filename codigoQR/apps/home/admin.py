9c39d2a8223bc03db7d66487055cdd122a4988da
alka653 <alka65@hotmail.com>
2018-10-14 00:13:19 -0500

    generate code qr

--- codigoQR/apps/home/admin.py
+++ codigoQR/apps/home/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 
 from.models import *
 
-admin.site.register(Estudiantes)
 admin.site.register(Registros)
 admin.site.register(Grados)
 
+class EstudiantesAdmin(admin.ModelAdmin):
+	fields = ('identificacion', 'nombre', 'grado')
+	list_display = ('identificacion', 'nombre', 'grado', 'codigoQR')
+
+	def codigoQR(self, obj):
+		return '<div id="id-'+obj.identificacion+'"><button id="btn-'+obj.identificacion+'" style="padding: 8px; border: none; color: white; background-color: #999; cursor: pointer; border-radius: 15px;" type="button" onclick="handleCodigoQR('+obj.identificacion+')">Generar QR</button></div>'
+
+	codigoQR.allow_tags = True
+
+admin.site.register(Estudiantes, EstudiantesAdmin)
