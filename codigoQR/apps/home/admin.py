fdbc752f54a2732a9170f95b6343294f998a3ece
alka653 <alka65@hotmail.com>
2018-10-14 00:29:42 -0500

    seacrg

--- codigoQR/apps/home/admin.py
+++ codigoQR/apps/home/admin.py
@@ -8,6 +8,7 @@ admin.site.register(Grados)
 class EstudiantesAdmin(admin.ModelAdmin):
 	fields = ('identificacion', 'nombre', 'grado')
 	list_display = ('identificacion', 'nombre', 'grado', 'codigoQR')
+	search_fields = ('identificacion', 'nombre', 'grado')
 
 	def codigoQR(self, obj):
 		return '<div id="id-'+obj.identificacion+'"><button id="btn-'+obj.identificacion+'" style="padding: 8px; border: none; color: white; background-color: #999; cursor: pointer; border-radius: 15px;" type="button" onclick="handleCodigoQR('+obj.identificacion+')">Generar QR</button></div>'
